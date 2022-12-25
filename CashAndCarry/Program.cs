ce328d892a8f9aae7f6cd73f33e1104a76e105a2
Сергей Усачев <usachevplayer@mail.ru>
2022-12-25 06:13:01 +0200

    tinal

--- CashAndCarry/Program.cs
+++ CashAndCarry/Program.cs
@@ -5,7 +5,7 @@
 		static void Main(string[] args)
 		{
 			int shopAccount = 0;
-			int[] clientsData = new int[] { 1200, 5600, 2400, 5200, 12, 2, 10050 };
+			int[] clientsData = new int[] {1200, 5600, 2400, 5200, 12, 2, 10050};
 			Queue<int> clients = new Queue<int>();
 			foreach (var item in clientsData)
 			{
