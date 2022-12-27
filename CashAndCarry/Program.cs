c825f7c7c81907b302a35ac7ff4ae7d5cf14b5fa
Сергей Усачев <usachevplayer@mail.ru>
2022-12-27 04:18:15 +0200

    поправил строки

--- CashAndCarry/Program.cs
+++ CashAndCarry/Program.cs
@@ -7,21 +7,19 @@
 			int shopAccount = 0;
 			int[] clientsData = new int[] {1200, 5600, 2400, 5200, 12, 2, 10050};
 			Queue<int> clients = new Queue<int>();
+
 			foreach (var item in clientsData)
 			{
 				clients.Enqueue(item);
 			}
 
-			for (int i = clients.Count - 1; i >= 0; i--)
+			while (clients.Count>0)
 			{
 				shopAccount+= clients.Peek();
 				Console.WriteLine(shopAccount);
 				clients.Dequeue();
 				Console.ReadKey();
-
 			}
-
-
 		}
 	}
 }
